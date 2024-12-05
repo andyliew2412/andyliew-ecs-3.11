7be7eb39afb9bd9c53a839fc4cf2adac3d49bc48
andyliew2412 <andy.liewchunkit@gmail.com>
2024-12-05 16:10:53 +0800

    update backend.tf file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sctp-c7-tfstate-bucket-1"
+    bucket = "sctp-ce7-tfstate"
     key    = "ecs-fargate-andyliew.tfstate" # Change the value of this to yourname-tf-workspace-act.tfstate for  example
     region = "us-east-1"
   }
